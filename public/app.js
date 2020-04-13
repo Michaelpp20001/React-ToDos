28b21746e0e8200ca20f8f935dc91e69f462ffb3
Michael <michaelpp2001@gmail.com>
2020-04-13 12:49:49 -0700

    added delete functionality

--- public/app.js
+++ public/app.js
@@ -8,6 +8,20 @@ $(document).ready(function(){
             console.log("you hit enter!");
             createTodo()
         }
+    });
+    //on load, listening to clicks on list, and dynamically generated span, happens only
+    //because we put the click listener on the ul's or on list class
+    $(".list").on("click", "span", function(){
+        var clickedId = $(this).parent().data("id");
+        var deleteUrl = "/api/todos/" + clickedId;
+        $(this).parent().remove();
+        $.ajax({
+            method: "DELETE",
+            url: deleteUrl
+        })
+        .then(function(data){
+            console.log(data);
+        })
     })
 });
 
@@ -20,7 +34,9 @@ function addTodos(todo){
 };
 
 function addTodo(todo){
-    var newTodo = $("<li class='task'>" + todo.name + todo.completed + "</li>");
+    var newTodo = $("<li class='task'>" + todo.name + " <span>X</span></li>");
+    //jQuery is storing the id of each new todo
+    newTodo.data("id", todo._id);
     //toggle, or adding a new class for the todo if completed
     if(todo.completed)
         newTodo.addClass("done")
@@ -39,7 +55,6 @@ function createTodo(){
         $("#todoInput").val("");
         //calling previously made function to append new todo
         addTodo(newTodo);
-
     })
     .catch(function(err){
         console.log(err);

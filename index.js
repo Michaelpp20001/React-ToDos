8ea5b092b9e15ccbc50a3ba7cc61a29157848c9a
Michael <michaelpp2001@gmail.com>
2020-04-13 11:03:42 -0700

    added html, css files and code

--- index.js
+++ index.js
@@ -5,12 +5,17 @@ var express = require("express"),
 
 var todoRoutes = require("./routes/todos");
 
-//allow us to access the request body
+//allow us to access the request body, had to npm install body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+//allow us to point to html file for the view
+//telling express to serve these static files and these are the directories
+app.use(express.static(__dirname + "/views"));
+//allow us to point to css file in the public directory for the styles
+app.use(express.static(__dirname + "/public"));
 
 app.get('/', function(req, res){
-    res.send("Hello from the root route");
+    res.sendFile("index.html");
 })
 
 app.use("/api/todos", todoRoutes);
